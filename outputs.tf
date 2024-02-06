3ba0c0ae16e88039814b703c2320327435a41257
Daniel Milanov <daniel.milanov@itgix.com>
2024-02-06 11:42:14 +0200

    Init module scaffold

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "eks_cluster_id" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_arn" {
+  value = module.eks.cluster_arn
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_version" {
+  value = module.eks.cluster_version
+}
