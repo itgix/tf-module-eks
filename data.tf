5862f02f2e0dd646433fbcb035c2351837a5d27e
Yordan Todorov <yordan.todorov@itgix.com>
2024-02-09 13:20:04 +0200

    fixing typo

--- data.tf
+++ data.tf
@@ -2,6 +2,6 @@ data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
 }
 data "aws_eks_cluster_auth" "this" {
-  name = mmodule.eks.cluster_name
+  name = module.eks.cluster_name
 }
 data "aws_caller_identity" "current" {}
