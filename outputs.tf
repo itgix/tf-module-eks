05d8a296380f43e755038e55c692fd130dbd869d
Yordan Todorov <yordan.todorov@itgix.com>
2024-02-09 15:39:02 +0200

    fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
 output "eks_cluster_version" {
   value = module.eks.cluster_version
 }
