065ed8412292b7989d0157635d8ab4d99e35a1fa
Mihail Vukadinoff <mihail.vukadinoff@itgix.com>
2024-02-12 10:45:16 +0200

    revert eks mod version to 19 as aws_roles unsupported in 20

--- eks.tf
+++ eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.2.1"
+  version = "19.21.0"
 
   cluster_name                         = var.eks_cluster_name
   cluster_version                      = var.eks_cluster_version
