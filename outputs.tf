91fccf378bc1a0415c0537c0be6a9445dd9e3b8a
Yordan Todorov <yordan.todorov@itgix.com>
2024-04-03 14:20:32 +0300

    fixing output module eks index

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "eks_cluster_id" {
-  value = module.eks.cluster_name
+  value = module.eks[0].cluster_name
 }
 
 output "eks_cluster_arn" {
-  value = module.eks.cluster_arn
+  value = module.eks[0].cluster_arn
 }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = module.eks[0].cluster_endpoint
 }
 
 output "eks_cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+  value = module.eks[0].cluster_certificate_authority_data
 }
 
 output "eks_cluster_version" {
-  value = module.eks.cluster_version
+  value = module.eks[0].cluster_version
 }
 
 output "eks_irsa_external_dns_arn" {
