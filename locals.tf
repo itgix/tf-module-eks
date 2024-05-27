45421f016cc8770e7985d910cfdd41c11052850a
Daniel Milanov <daniel.milanov@itgix.com>
2024-05-27 13:11:06 +0300

    Karpenter submodule

---
+++ locals.tf
@@ -0,0 +1,4 @@
+  karpenter_queue_name           = "queue-${var.aws_region}-${var.environment}-karpenter"
+  karpenter_namespace            = "karpenter"
+  karpenter_service_account_name = "karpenter"
+
