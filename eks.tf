def6accd26f92bc10fc0bd2a1c822c8179564ee3
Hristiyan Tonev <41945933+hvtonev@users.noreply.github.com>
2025-04-03 14:21:40 +0300

    test eks-auto mode

--- eks.tf
+++ eks.tf
@@ -17,18 +17,6 @@ module "eks" {
   cluster_enabled_log_types              = var.cluster_enabled_log_types
   cloudwatch_log_group_retention_in_days = var.cluster_log_retention_in_days
 
-  cluster_addons = {
-    coredns = {
-      addon_version = var.addons_versions.coredns
-    }
-    kube-proxy = {
-      addon_version = var.addons_versions.kube_proxy
-    }
-    vpc-cni = {
-      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
-    }
-  }
-
   cluster_security_group_additional_rules = {
     egress_nodes_ephemeral_ports_tcp = {
       description                = "To node 1025-65535"
@@ -38,6 +26,11 @@ module "eks" {
       type                       = "egress"
       source_node_security_group = true
     }
+  }
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
 
   node_security_group_additional_rules = {
@@ -59,68 +52,8 @@ module "eks" {
   control_plane_subnet_ids = var.control_plane_subnet_ids
 
 
-  # EKS Managed Node Group(s)
-
-  eks_managed_node_group_defaults = {
-    ami_type       = var.eks_ami_type
-    disk_size      = var.eks_disk_size
-    instance_types = var.eks_instance_types
-
-    iam_role_attach_cni_policy = true
-
-    iam_role_additional_policies = var.eks_node_additional_policies
-
-    block_device_mappings = {
-      xvda = {
-        device_name = "/dev/xvda"
-        ebs = {
-          volume_size           = var.eks_disk_size
-          volume_type           = var.eks_volume_type
-          iops                  = var.eks_volume_iops
-          throughput            = 150
-          encrypted             = true
-          delete_on_termination = true
-        }
-      }
-    }
-  }
-
-  eks_managed_node_groups = {
-    eks_workers = {
-      name         = "${var.eks_cluster_name}-ng"
-      min_size     = var.eks_ng_min_size
-      max_size     = var.eks_ng_max_size
-      desired_size = var.eks_ng_desired_size
-
-      ebs_optimized = true
-
-      metadata_options = {
-        http_endpoint               = "enabled"
-        http_tokens                 = "required"
-        http_put_response_hop_limit = 2
-        instance_metadata_tags      = "disabled"
-      }
-
-      subnet_ids            = var.subnet_ids
-      capacity_type         = var.eks_ng_capacity_type
-      create_security_group = true
-      security_group_name   = "${var.eks_cluster_name}-ng-sg"
-    }
-  }
-
   tags                          = var.eks_tags
   kms_key_enable_default_policy = var.kms_key_enable_default_policy
   kms_key_users                 = var.kms_key_users
 
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = var.addons_versions.ebs_csi
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = merge(
-    var.eks_tags,
-    tomap({ eks_addon = "ebs_csi" })
-  )
 }
--- variables.tf
+++ variables.tf
@@ -71,15 +71,6 @@ variable "cluster_log_retention_in_days" {
   default     = 14
 }
 
-variable "addons_versions" {
-  type = object({
-    kube_proxy = string
-    vpc_cni    = string
-    coredns    = string
-    ebs_csi    = string
-  })
-}
-
 variable "eks_tags" {
   type    = map(string)
   default = {}
@@ -99,72 +90,6 @@ variable "access_entries" {
   type        = any
   description = "Map of access entries to add to the cluster"
   default     = {}
-}
-
-################################################################################
-# Node group defaults 
-################################################################################
-
-variable "eks_ami_type" {
-  description = "Default AMI type for the EKS worker nodes"
-  type        = string
-  default     = "AL2_x86_64"
-}
-
-variable "eks_disk_size" {
-  description = "Disk size of the root volume attached to the EKS worker nodes"
-  type        = number
-  default     = 50
-}
-
-variable "eks_instance_types" {
-  description = "EC2 instance types for the EKS worker nodes"
-  type        = list(string)
-  default     = ["m5a.4xlarge"]
-}
-
-variable "eks_volume_type" {
-  description = "Type of the root EBS volume attached to the EKS worker nodes"
-  type        = string
-  default     = "gp3"
-}
-
-variable "eks_volume_iops" {
-  description = "Number of IOPs on the root EBS volumes"
-  type        = number
-  default     = 3000
-}
-
-variable "eks_node_additional_policies" {
-  description = "Additional policies to attach to the EKS worker nodes IAM role"
-  type        = map(string)
-  default = {
-    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-}
-
-variable "eks_ng_min_size" {
-  description = "Minimum number of the worker nodes in the node group"
-  type        = number
-  default     = 2
-}
-
-variable "eks_ng_max_size" {
-  description = "Maximum number of the worker nodes in the node group"
-  type        = number
-  default     = 5
-}
-
-variable "eks_ng_desired_size" {
-  description = "Desired number of the worker nodes in the node group"
-  type        = number
-  default     = 2
-}
-
-variable "eks_ng_capacity_type" {
-  description = "capacity type for node group nodes"
-  type        = string
-  default     = "SPOT"
 }
 
 variable "kms_key_enable_default_policy" {
