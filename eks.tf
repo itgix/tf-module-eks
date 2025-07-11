402da2d4cff795ee8e242b8ac5ccc9dc9c6b45f9
Hristiyan Tonev <41945933+hvtonev@users.noreply.github.com>
2025-07-11 09:50:46 +0300

    issue-246: support longer project name

--- eks.tf
+++ eks.tf
@@ -2,6 +2,9 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.31.6"
 
+  iam_role_use_name_prefix      = false
+  node_iam_role_use_name_prefix = false
+
   cluster_name                         = var.eks_cluster_name
   cluster_version                      = var.eks_cluster_version
   cluster_endpoint_private_access      = true
